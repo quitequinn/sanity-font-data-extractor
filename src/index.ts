dc0f3ef7a6971ac5cbce7e85e1a393c3a4022375
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 12:08:20 -0700

    Add index file to export FontDataExtractor component

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Sanity Font Data Extractor - Main Export
+ * Extract and analyze font data from documents with detailed typography information
+ */
+
+export { default as FontDataExtractor } from './FontDataExtractor'
+export { default } from './FontDataExtractor'
+
+// Export types if needed
+export type {
+  FontDataExtractorProps,
+  FontData,
+  ExtractionResult
+} from './FontDataExtractor'
